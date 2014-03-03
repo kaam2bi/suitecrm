3f0e38bb81f6a9d5fe5ebc13b32890e871aa6ea1
Jose Luis Cebrián Márquez <cebrianmarquez@gmail.com>
2014-03-03 10:10:58 +0100

    Commit inicial, con módulo Regoluna y SuiteCRM última versión con actualización de idioma en Español.

---
+++ custom/Extension/application/Ext/Include/reg_invoices.php
@@ -0,0 +1,12 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['fact_Facturas'] = 'fact_Facturas';
+$beanFiles['fact_Facturas'] = 'modules/fact_Facturas/fact_Facturas.php';
+$moduleList[] = 'fact_Facturas';
+$beanList['fact_Items'] = 'fact_Items';
+$beanFiles['fact_Items'] = 'modules/fact_Items/fact_Items.php';
+$modules_exempt_from_availability_check['fact_Items'] = 'fact_Items';
+$report_include_modules['fact_Items'] = 'fact_Items';
+$modInvisList[] = 'fact_Items';
+
+?>
---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,27 @@
+<?php
+// created: 2014-02-17 09:02:04
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_linkedin' => 'ext_rest_linkedin',
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_linkedin' => 'ext_rest_linkedin',
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_linkedin' => 'ext_rest_linkedin',
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Prospects' => 
+  array (
+    'ext_rest_linkedin' => 'ext_rest_linkedin',
+  ),
+  'Opportunities' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+);
