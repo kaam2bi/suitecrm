e0b20f43e54b18c2f82d3588432ee9ce4d15e2d2
Jose Luis Cebrián Márquez <cebrianmarquez@gmail.com>
2014-03-21 08:52:41 +0100

    Controlado el guardado de la imagen al original
    Sin redimensionar.

--- mobile/scripts/create_edit.js
+++ mobile/scripts/create_edit.js
@@ -5,32 +5,26 @@
 
 	var editionEnabled = true;
 	var lastNewNoteId = "";
+	var imageFile = null;
 
 	// Métodos onclick de boton de edición
 	$("a#SaveNewNote").click(function(event){ SugarCrmSetNewNote(); });
    	
-   	$('#fileInput').on('change', function() {
+   	// Método onchange de imagen
+   	$('#imageInput').on('change', function() {
    		
-   		var fileInput = document.getElementById('fileInput');	    	
+   		var fileInput = document.getElementById('imageInput');	    	
 	    
-   		var file = fileInput.files[0];
-	    // MegaPixImage constructor accepts File/Blob object.
+   		var file = fileInput.files[0]; // MegaPixImage constructor accepts File/Blob object.
 	    var mpImg = new MegaPixImage(file);
 
 	    // Render resized image into image element using quality option.
 	    // Quality option is valid when rendering into image element.
 	    var resImg = document.getElementById('attachedImage');
-	    mpImg.render(resImg, { maxWidth: 300, maxHeight: 300, quality: 0.8 });
+	    mpImg.render(resImg, {quality: 0.9 });
+	    imageFile = resImg.toDataURL("image/jpeg", 0.9);
 
-   		/***
-
-
-	    var file = $('#fileInput').files[0];
-	    
-	    var mpImg = new MegaPixImage(file); // MegaPixImage constructor accepts File/Blob object.
-	    var resImg = $('#attachedImage');
-	    mpImg.render(resImg, { maxWidth: 300, maxHeight: 300, quality: 0.8 });
-   		*/
+	    console.log(imageFile);
     });
 
 	function SugarCrmSetNewNote()
